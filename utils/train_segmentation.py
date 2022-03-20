a1b12eb76502f1e2bc93f58b83c035f3e488b056
Nicolas Gong <gong.qifeng@qq.com>
2022-03-20 22:59:27 +0800

    run segmentation

--- utils/train_segmentation.py
+++ utils/train_segmentation.py
@@ -76,68 +76,68 @@ scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=20, gamma=0.5)
 classifier.cuda()
 
 num_batch = len(dataset) / opt.batchSize
-
-for epoch in range(opt.nepoch):
-    scheduler.step()
-    for i, data in enumerate(dataloader, 0):
-        points, target = data
-        points = points.transpose(2, 1)
-        points, target = points.cuda(), target.cuda()
-        optimizer.zero_grad()
-        classifier = classifier.train()
-        pred, trans, trans_feat = classifier(points)
-        pred = pred.view(-1, num_classes)
-        target = target.view(-1, 1)[:, 0] - 1
-        #print(pred.size(), target.size())
-        loss = F.nll_loss(pred, target)
-        if opt.feature_transform:
-            loss += feature_transform_regularizer(trans_feat) * 0.001
-        loss.backward()
-        optimizer.step()
-        pred_choice = pred.data.max(1)[1]
-        correct = pred_choice.eq(target.data).cpu().sum()
-        print('[%d: %d/%d] train loss: %f accuracy: %f' % (epoch, i, num_batch, loss.item(), correct.item()/float(opt.batchSize * 2500)))
-
-        if i % 10 == 0:
-            j, data = next(enumerate(testdataloader, 0))
+if __name__=="__main__":
+    for epoch in range(opt.nepoch):
+        scheduler.step()
+        for i, data in enumerate(dataloader, 0):
             points, target = data
             points = points.transpose(2, 1)
             points, target = points.cuda(), target.cuda()
-            classifier = classifier.eval()
-            pred, _, _ = classifier(points)
+            optimizer.zero_grad()
+            classifier = classifier.train()
+            pred, trans, trans_feat = classifier(points)
             pred = pred.view(-1, num_classes)
             target = target.view(-1, 1)[:, 0] - 1
+            #print(pred.size(), target.size())
             loss = F.nll_loss(pred, target)
+            if opt.feature_transform:
+                loss += feature_transform_regularizer(trans_feat) * 0.001
+            loss.backward()
+            optimizer.step()
             pred_choice = pred.data.max(1)[1]
             correct = pred_choice.eq(target.data).cpu().sum()
-            print('[%d: %d/%d] %s loss: %f accuracy: %f' % (epoch, i, num_batch, blue('test'), loss.item(), correct.item()/float(opt.batchSize * 2500)))
+            print('[%d: %d/%d] train loss: %f accuracy: %f' % (epoch, i, num_batch, loss.item(), correct.item()/float(opt.batchSize * 2500)))
 
-    torch.save(classifier.state_dict(), '%s/seg_model_%s_%d.pth' % (opt.outf, opt.class_choice, epoch))
+            if i % 10 == 0:
+                j, data = next(enumerate(testdataloader, 0))
+                points, target = data
+                points = points.transpose(2, 1)
+                points, target = points.cuda(), target.cuda()
+                classifier = classifier.eval()
+                pred, _, _ = classifier(points)
+                pred = pred.view(-1, num_classes)
+                target = target.view(-1, 1)[:, 0] - 1
+                loss = F.nll_loss(pred, target)
+                pred_choice = pred.data.max(1)[1]
+                correct = pred_choice.eq(target.data).cpu().sum()
+                print('[%d: %d/%d] %s loss: %f accuracy: %f' % (epoch, i, num_batch, blue('test'), loss.item(), correct.item()/float(opt.batchSize * 2500)))
 
-## benchmark mIOU
-shape_ious = []
-for i,data in tqdm(enumerate(testdataloader, 0)):
-    points, target = data
-    points = points.transpose(2, 1)
-    points, target = points.cuda(), target.cuda()
-    classifier = classifier.eval()
-    pred, _, _ = classifier(points)
-    pred_choice = pred.data.max(2)[1]
+        torch.save(classifier.state_dict(), '%s/seg_model_%s_%d.pth' % (opt.outf, opt.class_choice, epoch))
 
-    pred_np = pred_choice.cpu().data.numpy()
-    target_np = target.cpu().data.numpy() - 1
+    ## benchmark mIOU
+    shape_ious = []
+    for i,data in tqdm(enumerate(testdataloader, 0)):
+        points, target = data
+        points = points.transpose(2, 1)
+        points, target = points.cuda(), target.cuda()
+        classifier = classifier.eval()
+        pred, _, _ = classifier(points)
+        pred_choice = pred.data.max(2)[1]
 
-    for shape_idx in range(target_np.shape[0]):
-        parts = range(num_classes)#np.unique(target_np[shape_idx])
-        part_ious = []
-        for part in parts:
-            I = np.sum(np.logical_and(pred_np[shape_idx] == part, target_np[shape_idx] == part))
-            U = np.sum(np.logical_or(pred_np[shape_idx] == part, target_np[shape_idx] == part))
-            if U == 0:
-                iou = 1 #If the union of groundtruth and prediction points is empty, then count part IoU as 1
-            else:
-                iou = I / float(U)
-            part_ious.append(iou)
-        shape_ious.append(np.mean(part_ious))
+        pred_np = pred_choice.cpu().data.numpy()
+        target_np = target.cpu().data.numpy() - 1
 
-print("mIOU for class {}: {}".format(opt.class_choice, np.mean(shape_ious)))
+        for shape_idx in range(target_np.shape[0]):
+            parts = range(num_classes)#np.unique(target_np[shape_idx])
+            part_ious = []
+            for part in parts:
+                I = np.sum(np.logical_and(pred_np[shape_idx] == part, target_np[shape_idx] == part))
+                U = np.sum(np.logical_or(pred_np[shape_idx] == part, target_np[shape_idx] == part))
+                if U == 0:
+                    iou = 1 #If the union of groundtruth and prediction points is empty, then count part IoU as 1
+                else:
+                    iou = I / float(U)
+                part_ious.append(iou)
+            shape_ious.append(np.mean(part_ious))
+
+    print("mIOU for class {}: {}".format(opt.class_choice, np.mean(shape_ious)))
